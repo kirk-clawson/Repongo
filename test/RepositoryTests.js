8d206ef75102d3ed24552ac46d9aeebd31c08326
Kirk Clawson <kirk.clawson@varkden.com>
2016-02-23 22:29:19 -0500

    Implemented Promise interface to repo and updated tests

--- test/RepositoryTests.js
+++ test/RepositoryTests.js
@@ -4,29 +4,36 @@
 var repongo = require('../main.js');
 var should = require('should');
 
-describe('With an empty Repository,', function(){
+describe('With an empty Repository,', function () {
 
     var db = repongo('mongodb://localhost/repongo_test');
     var catSchema = new db.Schema('cat');
     var repoUnderTest = db.createRepo(catSchema);
 
-    describe('when a cat is inserted', function(){
-        it('Inserts one record that can be read back', function(done){
-            repoUnderTest.save({ name: 'Mr. Puggles', age: 6}, function(err, result){
-                should.not.exist(err);
-                should.exist(result);
-                result.should.have.property('_id');
-                repoUnderTest.getAll(function(err, result){
-                    should.not.exist(err);
+    describe('when a cat is inserted', function () {
+        it('Inserts one record that can be read back', function (done) {
+            var id;
+            repoUnderTest.save({name: 'Mr. Puggles', age: 6})
+                .then(function (result) {
+                    should.exist(result);
+                    result.should.have.property('_id');
+                    id = result._id;
+                    return repoUnderTest.getAll();
+                })
+                .then(function (result) {
                     should.exist(result);
                     result.length.should.equal(1);
+                    id.should.equal(result[0]._id);
+                    done();
+                })
+                .catch(function (err) {
+                    should.not.exist(err);
                     done();
                 });
-            });
         });
     });
 
-    afterEach(function(){
+    afterEach(function () {
         repoUnderTest._clear();
     });
 });
