05a78f30bf68ac64af00e5358b027629dbdf5d36
kirk-clawson <kirk.clawson@outlook.com>
2016-03-09 10:40:34 -0500

    Renamed floatField to numberField and changed intField to extend numberField

--- lib/fields/numberField.ts
+++ lib/fields/numberField.ts
@@ -4,15 +4,15 @@ import {IField, IFluentValidator, FieldRule} from './base';
 import {stringFormat} from '../util';
 import {AnyImpl} from './anyField';
 
-interface IFloatFluent extends IFluentValidator<IFloatFluent> {
-    hasMaximumOf(value: number, message?: string): IFloatFluent;
-    hasMinimumOf(value: number, message?: string): IFloatFluent;
+interface INumberFluent extends IFluentValidator<INumberFluent> {
+    hasMaximumOf(value: number, message?: string): INumberFluent;
+    hasMinimumOf(value: number, message?: string): INumberFluent;
 }
 
-class FloatImpl extends AnyImpl implements IField, IFloatFluent {
+class NumberImpl extends AnyImpl implements IField, INumberFluent {
     static defaultMinMessage: string = '?0: value of ?1: is less than the minimum value of ?2:';
     static defaultMaxMessage: string = '?0: value of ?1: exceeds the maximum value of ?2:';
-    static defaultTypeMessage: string = '?0: does not match the specified data type (Float)';
+    static defaultTypeMessage: string = '?0: does not match the specified data type (Number)';
 
     max: FieldRule<number>;
     min: FieldRule<number>;
@@ -21,10 +21,10 @@ class FloatImpl extends AnyImpl implements IField, IFloatFluent {
     constructor();
     constructor(defaultValue: number);
     constructor(defaultValue?: number, message?: string);
-    constructor(defaultValue: number = 0, message: string = FloatImpl.defaultTypeMessage) {
+    constructor(defaultValue: number = 0, message: string = NumberImpl.defaultTypeMessage) {
         super(_.isNil(defaultValue) ? 0 : defaultValue);
-        this.max = new FieldRule<number>(null, FloatImpl.defaultMaxMessage);
-        this.min = new FieldRule<number>(null, FloatImpl.defaultMinMessage);
+        this.max = new FieldRule<number>(null, NumberImpl.defaultMaxMessage);
+        this.min = new FieldRule<number>(null, NumberImpl.defaultMinMessage);
         this.dataTypeMessage = message;
     }
 
@@ -45,26 +45,28 @@ class FloatImpl extends AnyImpl implements IField, IFloatFluent {
         return result;
     }
 
-    isRequired(): IFloatFluent;
-    isRequired(message?: string): IFloatFluent;
-    isRequired(message: string = AnyImpl.defaultRequiredMessage): IFloatFluent {
+    isRequired(): INumberFluent;
+    isRequired(message?: string): INumberFluent;
+    isRequired(message: string = AnyImpl.defaultRequiredMessage): INumberFluent {
         super.isRequired(message);
         return this;
     }
 
-    hasMaximumOf(value: number): IFloatFluent;
-    hasMaximumOf(value: number, message: string = FloatImpl.defaultMaxMessage): IFloatFluent {
+    hasMaximumOf(value: number): INumberFluent;
+    hasMaximumOf(value: number, message?: string): INumberFluent;
+    hasMaximumOf(value: number, message: string = NumberImpl.defaultMaxMessage): INumberFluent {
         this.max.value = value;
         this.max.setNonNullMessage(message);
         return this;
     }
 
-    hasMinimumOf(value: number): IFloatFluent;
-    hasMinimumOf(value: number, message: string = FloatImpl.defaultMinMessage): IFloatFluent {
+    hasMinimumOf(value: number): INumberFluent;
+    hasMinimumOf(value: number, message?: string): INumberFluent;
+    hasMinimumOf(value: number, message: string = NumberImpl.defaultMinMessage): INumberFluent {
         this.min.value = value;
         this.min.setNonNullMessage(message);
         return this;
     }
 }
 
-export { IFloatFluent, FloatImpl };
+export { INumberFluent, NumberImpl };
